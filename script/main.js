785ee473161e2e1895512f2e779c37c62737c65d
leafweb <leafweb.io@gmail.com>
2024-08-14 19:38:48 +0330

    update

--- script/main.js
+++ script/main.js
@@ -15,24 +15,48 @@ let RemoveClass = (x, y) => {
 let ToggleClass = (x, y) => {
    x.classList.toggle(y);
 }
-let Fab = ()=> {
+let Fab = () => {
    if (event.currentTarget.classList.contains('hide')) {
-      window.scrollTo({top: 0,behavior: 'smooth'});
+      window.scrollTo({ top: 0, behavior: 'smooth' });
    } else {
-      window.open('menu.html','_self');
+      window.open('menu.html', '_self');
    }
 }
-let Menu = ()=> {
-   ToggleClass(menu,'show');
-   ToggleClass(menuBtn,'show');
+let Menu = () => {
+   ToggleClass(menu, 'show');
+   ToggleClass(menuBtn, 'show');
 }
-window.addEventListener('scroll', ()=> {
-  let scrolled = window.scrollY;
-  if (scrolled >= 250) {
-    RemoveClass(header,'hide');
-    AddClass(fab,'hide');
-  } else {
-     AddClass(header,'hide');
-     RemoveClass(fab,'hide');
-  }
+window.addEventListener('scroll', () => {
+   let scrolled = window.scrollY;
+   if (scrolled >= 250) {
+      RemoveClass(header, 'hide');
+      AddClass(fab, 'hide');
+   } else {
+      AddClass(header, 'hide');
+      RemoveClass(fab, 'hide');
+   }
+});
+
+
+let elements = document.querySelectorAll('.scrollAnim');
+let callback = (entries, observer) => {
+   entries.forEach(entry => {
+      if (entry.isIntersecting) {
+         entry.target.classList.add('showAnim');
+      } else {
+         entry.target.classList.remove('showAnim');
+      }
+   });
+};
+
+const options = {
+   root: null,
+   rootMargin: '0px',
+   threshold: 0.1
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+elements.forEach(element => {
+   observer.observe(element);
 });
