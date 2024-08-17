ca030992a5faeb8decd5fef0cc40bcbb329f2de2
leafweb <leafweb.io@gmail.com>
2024-08-17 09:13:54 +0330

    update

---
+++ script/menu.js
@@ -0,0 +1,41 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu');
+let menuBtn = document.querySelector('#menuBtn');
+let AddClass = (x, y) => {
+   if (!x.classList.contains(y)) {
+      x.classList.add(y);
+   }
+}
+let RemoveClass = (x, y) => {
+   if (x.classList.contains(y)) {
+      x.classList.remove(y);
+   }
+}
+let ToggleClass = (x, y) => {
+   x.classList.toggle(y);
+}
+let Menu = () => {
+   ToggleClass(menu, 'show');
+   ToggleClass(menuBtn, 'show');
+}
+window.addEventListener('load', function() {
+   let elements = document.querySelectorAll('.scrollAnim');
+   let callback = (entries, observer) => {
+      entries.forEach(entry => {
+         if (entry.isIntersecting) {
+            entry.target.classList.add('showAnim');
+         } else {
+            entry.target.classList.remove('showAnim');
+         }
+      });
+   };
+   const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0
+   };
+   const observer = new IntersectionObserver(callback, options);
+   elements.forEach(element => {
+      observer.observe(element);
+   });
+});
